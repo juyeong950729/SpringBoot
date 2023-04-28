c07fde401e8fb0b1bcecd4285ef597c1380ef9f5
jojuyeong03 <jojuyeong03@gmail.com>
2023-04-28 19:43:05 +0900

    2023/04/28 voard

---
+++ Voard/src/main/java/kr/co/voard/controller/UserController.java
@@ -0,0 +1,101 @@
+package kr.co.voard.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import kr.co.voard.service.UserService;
+import kr.co.voard.vo.TermsVO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.voard.entity.UserEntity;
+import kr.co.voard.jwt.JwtUtil;
+import kr.co.voard.security.MyUserDetails;
+import kr.co.voard.security.SecurityUserService;
+import kr.co.voard.vo.UserVO;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@AllArgsConstructor
+@RestController
+public class UserController {
+
+	private AuthenticationManager authenticationManager;
+	private SecurityUserService securityUserService;
+	private JwtUtil jwtUtil;
+	private UserService service;
+
+	@GetMapping("/user/terms")
+	public TermsVO terms(){
+		return service.selectTerms();
+	}
+
+	@GetMapping("/user/checkUid")
+	public int checkUid(String uid){
+		return service.countUid(uid);
+	}
+
+	@PostMapping("/user/register")
+	public void register(@RequestBody UserVO vo){
+		service.insertUser(vo);
+	}
+
+	@GetMapping("/user")
+	public UserEntity user(Authentication authentication) {
+
+		log.info("user...1");
+		MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+
+		return myUserDetails.getUser();
+	}
+
+	@PostMapping("/user/login")
+	public Map<String, Object> login(@RequestBody UserVO vo) {
+		log.info("vo : " + vo);
+		String uid = vo.getUid();
+		String pass = vo.getPass();
+
+		log.info("login...1");
+
+		// Security 인증처리
+		MyUserDetails myUserDetails = securityUserService.loadUserByUsername(uid);
+		log.info("login...2");
+
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(myUserDetails, pass));
+		log.info("login...3");
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		// JWT 생성
+		String token = jwtUtil.generateToken(uid);
+		log.info("login...4 : " + token);
+
+		// JWT 출력
+		UserEntity user = myUserDetails.getUser();
+
+
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("accessToken", token);
+		resultMap.put("user", user);
+
+		return resultMap;
+	}
+
+
+	public void logout() {
+
+	}
+
+
+
+}
---
+++ Voard/src/main/java/kr/co/voard/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package kr.co.voard.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import kr.co.voard.security.MyUserDetails;
+import kr.co.voard.security.SecurityUserService;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private SecurityUserService securityUserService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = jwtUtil.resolveToken(request);
+
+        log.info("filter...1 : " + token);
+
+        if(token != null && jwtUtil.validateToken(token)) {
+
+            String uid = jwtUtil.getUsernameFromToken(token);
+            log.info("filter...2 : " + uid);
+
+            // Security 인증처리
+            MyUserDetails myUserDetails  = securityUserService.loadUserByUsername(uid);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(myUserDetails, null, myUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+
+        log.info("filter...3");
+        // 다음 필터 이동
+        filterChain.doFilter(request, response);
+
+    }
+
+}
